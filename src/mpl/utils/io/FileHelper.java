369117c706db3c62b93d0490e3a0ac22cbc780d5
druckis3000 <muodiux1@gmail.com>
2022-01-31 16:56:19 +0200

    Added function for generating SHA-1 checksum for files

--- src/mpl/utils/io/FileHelper.java
+++ src/mpl/utils/io/FileHelper.java
@@ -3,9 +3,13 @@ package mpl.utils.io;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class FileHelper {
 
@@ -52,5 +56,40 @@ public class FileHelper {
 	
 	public static String readFile(File file){
 		return readFile(file.getAbsolutePath());
+	}
+	
+	public static byte[] getSHA1(File file) {
+		try {
+			MessageDigest sha1Digest = MessageDigest.getInstance("SHA-1");
+			InputStream fis = new FileInputStream(file);
+			int n = 0;
+			byte[] buffer = new byte[8192];
+			while(n != -1) {
+				n = fis.read(buffer);
+				if(n > 0) {
+					sha1Digest.update(buffer, 0, n);
+				}
+			}
+			fis.close();
+			
+			return sha1Digest.digest();
+		} catch (NoSuchAlgorithmException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public static String getSHA1String(File file) {
+		byte[] sha1 = getSHA1(file);
+		String sha1Str = "";
+		if(sha1 != null) {
+			for(byte b : sha1) {
+				sha1Str += String.format("%02x", Math.abs(b));
+			}
+		}
+		return sha1Str;
 	}
 }
